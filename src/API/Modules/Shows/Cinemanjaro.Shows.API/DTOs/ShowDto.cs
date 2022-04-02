8d42b5a644be5b74a0d8b3f95d2a67c4e5234e53
Kacper <kacper916@gmail.com>
2022-04-03 00:15:05 +0200

    Show dto modified

--- src/API/Modules/Shows/Cinemanjaro.Shows.API/DTOs/ShowDto.cs
+++ src/API/Modules/Shows/Cinemanjaro.Shows.API/DTOs/ShowDto.cs
@@ -7,14 +7,14 @@ namespace Cinemanjaro.Shows.API.DTOs
         public string Id { get; init; }
         public DateTime Date { get; init; }
         public string Title { get; init; }
-        public IEnumerable<SeatDto> Positions { get; init; }    
+        public IEnumerable<SeatDto> Seats { get; init; }    
 
         public ShowDto(Show show)
         {
             Id = show.Id.ToString();
             Date = show.Date;
             Title = show.Title;
-            Positions = show.Seats.Select(x => new SeatDto(x));
+            Seats = show.Seats.Select(x => new SeatDto(x));
         }
     }
 }
